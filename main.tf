0ad9375b9acf3fcad419cde1db250b4456429576
Veerabathiran.T <veerabathiran.t@onixnet.com>
2025-03-05 01:19:26 +0530

    Creating main.tf provider.tf and variables.tf file for creating resources in the GCP.

--- main.tf
+++ main.tf
@@ -1,10 +1,4 @@
 resource "google_storage_bucket" "test-bucket" {
   name     = "your-unique-bucket-name"
   location = "US"
-
-  storage_class = "STANDARD"
-
-  versioning {
-    enabled = true
-  }
 }
