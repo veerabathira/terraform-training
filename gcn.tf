29221da71a9a60d4a98d1b98932bbc38e3a3a9c4
Veerabathiran.T <veerabathiran.t@onixnet.com>
2025-03-14 16:03:22 +0530

    Creating VMs and Network

--- gcn.tf
+++ gcn.tf
@@ -1,11 +1,12 @@
-# resource "google_compute_network" "gcn-1" {
-#   name = "first-network"
-#   auto_create_subnetworks = false
-# }
 
-# resource "google_compute_subnetwork" "sub-us" {
-#   name = "sub-us"
-#   network = google_compute_network.gcn-1.id
-#   ip_cidr_range = "10.1.0.0/24"
-#   region = "us-east1"
-# }
+resource "google_compute_network" "gcn-1" {
+  name = "first-network"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "sub-us" {
+  name = "sub-us"
+  network = google_compute_network.gcn-1.id
+  ip_cidr_range = "10.1.0.0/24"
+  region = "us-east1"
+}
